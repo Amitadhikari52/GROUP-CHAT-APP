9a48f15b973671c53739af382a20ae7e6e0ffa53
Amit Adhikari <amitadhikari525252@gmail.com>
2023-10-17 16:01:59 +0530

    Task-11 -Making Multiple Groups

--- controller/chatController.js
+++ controller/chatController.js
@@ -2,6 +2,7 @@ const path = require("path");
 const User = require("../models/userModel");
 
 const Chat = require("../models/chatModel");
+const Group = require("../models/groupModel");
 
 const sequelize = require("../util/db");
 
@@ -9,10 +10,14 @@ const { Op } = require("sequelize");
 
 exports.sendMessage = async (req, res, next) => {
   try {
+    const group = await Group.findOne({
+      where: { name: req.body.groupName },
+    });
     await Chat.create({
       name: req.user.name,
       message: req.body.message,
       userId: req.user.id,
+      groupId: group.dataValues.id,
     });
     return res.status(200).json({ message: "Success!" });
   } catch (error) {
@@ -23,11 +28,18 @@ exports.sendMessage = async (req, res, next) => {
 
 exports.getMessages = async (req, res, next) => {
   try {
-    const param = req.params.param;
+    const param = req.query.param;
+    console.log(req.query.groupName);
+    const group = await Group.findOne({
+      where: { name: req.query.groupName },
+    });
     const messages = await Chat.findAll({
       where: {
-        id: {
-          [Op.gt]: param,
+        [Op.and]: {
+          id: {
+            [Op.gt]: param,
+          },
+          groupId: group.dataValues.id,
         },
       },
     });
